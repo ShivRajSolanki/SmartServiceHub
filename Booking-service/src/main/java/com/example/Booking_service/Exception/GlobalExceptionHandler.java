12650b678f5ed3ae958bbc80df5c5140b5036ff6
ShivRajSolanki <shivrajsolanki6001@gmail.com>
2026-03-07 23:25:37 +0530

    added globssl exception handler

--- Booking-service/src/main/java/com/example/Booking_service/Exception/GlobalExceptionHandler.java
+++ Booking-service/src/main/java/com/example/Booking_service/Exception/GlobalExceptionHandler.java
@@ -5,11 +5,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
     @ExceptionHandler(ServiceNotFoundException.class)
-    public ResponseEntity<String> handleServiceNotFound(ServiceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, Object>> handleServiceNotFound(ServiceNotFoundException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("message", ex.getMessage());
+        response.put("status", 404);
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntime(RuntimeException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("message", ex.getMessage());
+        response.put("status", 400);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
